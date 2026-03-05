ce2bf7ac9cffd328f2299cd74679f70e44b8fc09
twingz226 <twingzmarfil@.com>
2026-03-05 14:17:03 +0800

    your message

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css', [
-      // Add any PostCSS plugins here
+       require('tailwindcss'),
    ]);
