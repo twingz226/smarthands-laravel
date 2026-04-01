762b07e0d3c10ad2d6b6687d2591535e7a1f0496
twingz226 <twingzmarfil@.com>
2026-04-01 13:15:16 +0800

    your message

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -12,17 +12,9 @@
                 </a>
             </div>
 
-            <div class="sidebar-collapse">
-                <a href="#" class="sidebar-collapse-icon">
-                    <i class="entypo-menu"></i>
-                </a>
-            </div>
 
-            <div class="sidebar-mobile-menu visible-xs">
-                <a href="#" class="with-animation">
-                    <i class="entypo-menu"></i>
-                </a>
-            </div>
+
+
         </header>
 
         <div class="sidebar-user-info">
